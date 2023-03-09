ab4b0a2e8089d928858f3c6c5f0a559adac5ba7c
Chris Nelson <superchrisnelson@gmail.com>
2023-03-09 16:34:50 -0500

    bumpety bump

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule LiveElements.MixProject do
   def project do
     [
       app: :live_elements,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       description: @description,
       start_permanent: Mix.env() == :prod,
